20024855a794a64ac403347a9d4968e7e0480507
krtuhin <tuhin.km2774@gmail.com>
2023-07-23 02:50:32 +0530

    Insert Query using Spring JDBC (Static and Dynamic)

--- src/main/java/com/springjdbc/SpringJDBC.java
+++ src/main/java/com/springjdbc/SpringJDBC.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.springjdbc;
 
-/**
- *
- * @author krtuh
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class SpringJDBC {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("com/config/config.xml");
+
+        //getting template object from IoC container
+        JdbcTemplate jdbcTemplate
+                = context.getBean("jdbcTemplate", JdbcTemplate.class);
+
+        //insert query (static)
+        String query = "insert into student(id, name, city) values(35653, 'Varun', 'Delhi')";
+
+        //insert query dynamic
+        String query1 = "insert into student(id, name, city) values(?,?,?)";
+
+        //fire query
+        int result = jdbcTemplate.update(query1, 678678, "Raju", "Kanpur");
+
+        System.out.println("Number of row inserted: " + result);
     }
 }
